a7a799f1bdd0404887d3c7fa46335e422d7978d0
yhj <yhj@gmail.com>
2015-07-04 19:50:45 -0500

    updata csv data

--- readcolumn_0621.m
+++ readcolumn_0621.m
@@ -116,8 +116,8 @@ for FN=1:filenumber
     for row=ban_axis(FN):psize_y
         for i=1:psize_x
             if(pixel(row,i)>4)
-                EI_down=double(EI_down)+double(10.5)*((double((1e-3))*double(rou(row,i)))^2.57)*((((ban_axis(FN)-row)-1)*Pixel_scale(FN) + Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2);
-                I_down=double(I_down)+double(((((ban_axis(FN)-row)-1)*Pixel_scale(FN) + Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2));
+                EI_down=double(EI_down)+double(10.5)*((double((1e-3))*double(rou(row,i)))^2.57)*((((row-ban_axis(FN))+1)*Pixel_scale(FN) - Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2);
+                I_down=double(I_down)+double(((((row-ban_axis(FN))+1)*Pixel_scale(FN) - Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2));
             end
         end
     end
