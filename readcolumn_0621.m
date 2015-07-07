cc4c7de3be21098ce64bc8152feaaa895d0b6bd5
yhj <yhj@gmail.com>
2015-07-06 22:19:30 -0500

    calculation of EI for projection

--- readcolumn_0621.m
+++ readcolumn_0621.m
@@ -41,6 +41,7 @@ for FN=1:filenumber
     filecontent = dicomread(filelist_aftercheck{FN});
     [psize_y,psize_x]=size(filecontent);
     EA(FN)=0;
+    EA_p(FN)=0;
     GA(FN)=0;
     pix_num(FN)=0;
     tmp_rou=0;
@@ -58,7 +59,7 @@ for FN=1:filenumber
         end
     end
     
-    [range_x,range_y]=find(pixel > Pthreshold);
+    [range_x,range_y]=find(pixel > Pthreshold);%find the max distance to neutral axis
     min_x(FN)=min(range_x);
     max_x(FN)=max(range_x);
     min_y(FN)=min(range_y);
@@ -66,13 +67,13 @@ for FN=1:filenumber
     ave_rou(FN)=tmp_rou/pix_num(FN);
     Pixel_scale(FN)=sqrt(CSV_CSA(FN)/pix_num(FN));
 
-    tmp_rou_p=0;
-    project_y=sum(pixel,2);
+   % tmp_rou_p=0; %projection
+    project_y=sum(pixel,2); %projection along row direction
     for i=1:psize_y
         if(project_y(i) > Pthreshold)
-            rou_p(i,FN)=(double(FUC)*(double(project_y(i))-double(INTERCEPT)))/double(SLOP);%mg/cm3
-            E_p(i,FN)=double(10.5)*(double((1e-3))*double(rou_p(i)))^2.57;
-            tmp_rou_p=tmp_rou_p+rou_p(i);
+            rou_p(i,FN)=(double(FUC)*(double(project_y(i))-double(INTERCEPT)))/double(SLOP);%mg/cm3 ????????
+            E_p(i,FN)=double(29.8)*(double((1e-3))*double(rou_p(i,FN)))^1.56; %the relation between E and areal BMD
+            %tmp_rou_p=tmp_rou_p+rou_p(i);
         end
     end
 
@@ -85,10 +86,20 @@ for FN=1:filenumber
              end
         end
     end
-
+        
     GA(FN)=EA(FN)/(2*(1+mu));
     EA=EA';
     GA=GA';
+    
+    for i=1:psize_y
+        if(project_y(i) > Pthreshold)
+            EA_p(FN)=EA_p(FN) + double(double(E_p(i,FN))*double(Pixel_scale(FN)^2));
+         end
+    end
+    GA_p(FN)=EA_p(FN)/(2*(1+mu));
+    EA_p=EA_p';
+    GA_p=GA_p';
+    
     
     loca=0;
     Minimum=1e20;
@@ -128,7 +139,7 @@ for FN=1:filenumber
     I_up=0;
     for row=1:(ban_axis(FN)-1)
         for i=1:psize_x
-            if(pixel(row,i)>4)
+            if(pixel(row,i)>Pthreshold)
                 EI_up=double(EI_up)+double(10.5)*((double((1e-3))*double(rou(row,i)))^2.57)*((((ban_axis(FN)-row)-1)*Pixel_scale(FN) + Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2);
                 I_up=double(I_up)+double(((((ban_axis(FN)-row)-1)*Pixel_scale(FN) + Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2));
             end
@@ -139,7 +150,7 @@ for FN=1:filenumber
     I_down=0;
     for row=ban_axis(FN):psize_y
         for i=1:psize_x
-            if(pixel(row,i)>4)
+            if(pixel(row,i)>Pthreshold)
                 EI_down=double(EI_down)+double(10.5)*((double((1e-3))*double(rou(row,i)))^2.57)*((((row-ban_axis(FN))+1)*Pixel_scale(FN) - Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2);
                 I_down=double(I_down)+double(((((row-ban_axis(FN))+1)*Pixel_scale(FN) - Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2));
             end
@@ -149,6 +160,29 @@ for FN=1:filenumber
     EI(FN)=EI_up+EI_down;
     I(FN)=I_up+I_down;
     Z_y(FN)=I(FN)/(d_max(FN)*Pixel_scale(FN));
+    
+    EI_p_up=0;
+    I_p_up=0;
+    for row=1:(ban_axis(FN)-1)
+        if(project_y(row)>Pthreshold)
+                EI_p_up=double(EI_p_up)+double(29.8)*((double((1e-3))*double(rou_p(row)))^1.56)*((((ban_axis(FN)-row)-1)*Pixel_scale(FN) + Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2);
+                I_p_up=double(I_p_up)+double(((((ban_axis(FN)-row)-1)*Pixel_scale(FN) + Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2));
+          end
+     end
+    
+
+    EI_p_down=0;
+    I_p_down=0;
+    for row=ban_axis(FN):psize_y
+        if(project_y(row)>Pthreshold)
+                EI_p_down=double(EI_p_down)+double(29.8)*((double((1e-3))*double(rou_p(row)))^1.56)*((((row-ban_axis(FN))+1)*Pixel_scale(FN) - Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2);
+                I_p_down=double(I_p_down)+double(((((row-ban_axis(FN))+1)*Pixel_scale(FN) - Pixel_scale(FN)/2)^2)*(Pixel_scale(FN)^2));
+         end
+    end
+    EI_p(FN)=EI_p_up+EI_p_down;
+    I_p(FN)=I_p_up+I_p_down;
+    Z_p_y(FN)=I_p(FN)/(d_max(FN)*Pixel_scale(FN));
+      
  end
 ave_rou=ave_rou';
 EI=EI';
@@ -160,6 +194,9 @@ ban_axis(FN)=ban_axis(FN)';
 [r3,p3]=corr(CSMI_inplane,EA)
 [r4,p4]=corr(CSMI_inplane,GA)
 [r5,p5]=corr(SM_inplane,Z_y)
+[r6,p6]=corr(EA_p,EA)
+[r7,p7]=corr(EI_p,EI)
+[r8,p8]=corr(I_p,I)
 
 
 
